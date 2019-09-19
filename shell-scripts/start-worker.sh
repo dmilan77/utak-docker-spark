3695aa86f21d67d93754c3bd99b68e5a633d665d
Milan Das <milan@ulap.co>
2019-09-18 22:21:59 -0400

    added livy

--- shell-scripts/start-worker.sh
+++ shell-scripts/start-worker.sh
@@ -6,5 +6,6 @@ then
    echo "SPARK_MASTER_URL not set" >> /opt/logs/run.log
 else
    sed -i "s#{{SPARK_MASTER_URL}}#$SPARK_MASTER_URL#" /opt/etc/supervisor/conf.d/supervisor-worker.conf
-/usr/bin/supervisord -n -c /opt/etc/supervisor/conf.d/supervisor-worker.conf
+   sleep 2
+   /usr/bin/supervisord -n -c /opt/etc/supervisor/conf.d/supervisor-worker.conf
 fi

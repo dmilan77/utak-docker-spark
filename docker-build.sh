f6ac2dce265f6b9d7dca197325b36cdafa283275
Milan Das <milan@ulap.co>
2019-09-22 21:51:45 -0400

    2.4.4-r2

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 export TAG=2.4.4-r2
-docker build -t dmilan/utak-docker-spark . --build-arg SPARK_VERSION=2.4.4  --build-arg LIVY_VERSION=0.6.0-incubating
+export SPARK_VERSION=2.4.4
+export LIVY_VERSION=0.6.0-incubating
+docker build -t dmilan/utak-docker-spark . --build-arg SPARK_VERSION=${SPARK_VERSION}  --build-arg LIVY_VERSION=${LIVY_VERSION}
 docker tag  dmilan/utak-docker-spark dmilan/utak-docker-spark:${TAG}

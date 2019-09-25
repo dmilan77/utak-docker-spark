bd9ea3b28875fe68190a569c5ca332959e8afd0f
Milan Das <milan@ulap.co>
2019-09-24 23:06:07 -0400

    2.4.4-r2.3

--- docker-build.sh
+++ docker-build.sh
@@ -4,8 +4,8 @@ export SPARK_VERSION=2.4.4
 export LIVY_VERSION=0.6.0-incubating
 export GCR_PROJECT_ID=utak-236419
 date > .cachebust 
-docker build -t dmilan/utak-docker-spark . --build-arg SPARK_VERSION=${SPARK_VERSION}  --build-arg LIVY_VERSION=${LIVY_VERSION} -f Dockerfile.debian
-# docker build -t dmilan/utak-docker-spark . --build-arg SPARK_VERSION=${SPARK_VERSION}  --build-arg LIVY_VERSION=${LIVY_VERSION} -f Dockerfile.alpine
+# docker build -t dmilan/utak-docker-spark . --build-arg SPARK_VERSION=${SPARK_VERSION}  --build-arg LIVY_VERSION=${LIVY_VERSION} -f Dockerfile.debian
+docker build -t dmilan/utak-docker-spark . --build-arg SPARK_VERSION=${SPARK_VERSION}  --build-arg LIVY_VERSION=${LIVY_VERSION} -f Dockerfile.alpine
 # docker tag  dmilan/utak-docker-spark dmilan/utak-docker-spark:${TAG}
 # docker tag  dmilan/utak-docker-spark gcr.io/${GCR_PROJECT_ID}/utak-docker-spark
 # docker tag gcr.io/${GCR_PROJECT_ID}/utak-docker-spark gcr.io/${GCR_PROJECT_ID}/utak-docker-spark:${TAG}
